af7dc5b76f59a9cf213de9c8794a2af75c342b99
Daniel Reissenberger <dreissenberger@atlassian.com>
2019-03-10 10:46:45 +1100

    Debug Message service

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const surfDataService = require('./surfDataService');
 exports.handler = async () => {
     const spotIdBondi = '5842041f4e65fad6a7708bf8';
     const surfSummary = await surfDataService.getSummary('Bondi', spotIdBondi);
-    messageService.send(surfSummary);
+    messageService.send('Good morning');
 
     let response = {
         statusCode: 200,
