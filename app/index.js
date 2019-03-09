df933682d84ac3a528f31cdfb1947b8490c169ec
Daniel Reissenberger <dreissenberger@atlassian.com>
2019-03-10 02:23:07 +1100

    Added ability for HTTP invocations through the API Gateway

--- app/index.js
+++ app/index.js
@@ -1,10 +1,18 @@
 const messageService = require('./messageService');
 const surfDataService = require('./surfDataService');
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event) => {
     const spotIdBondi = '5842041f4e65fad6a7708bf8';
     const surfSummary = await surfDataService.getSummary('Bondi', spotIdBondi);
     messageService.send(surfSummary);
 
-    callback(null, surfSummary);
+    let response = {
+        statusCode: responseCode,
+        headers: {
+            "x-custom-header" : "my custom header value"
+        },
+        body: surfSummary
+    };
+    console.log("response: " + JSON.stringify(response))
+    return response;
 };
