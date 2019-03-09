f4aba859fb7fe66edf222198a75eec5ebf3d359c
Daniel Reissenberger <dreissenberger@atlassian.com>
2019-03-10 00:40:04 +1100

    Added await to surfDataService call

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
 const messageService = require('./messageService');
 const surfDataService = require('./surfDataService');
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
     const spotIdBondi = '5842041f4e65fad6a7708bf8';
-    const surfSummary = surfDataService.getSummary('Bondi', spotIdBondi);
+    const surfSummary = await surfDataService.getSummary('Bondi', spotIdBondi);
     messageService.send(surfSummary);
 
     callback(null, 'Hello from Lambda feat. Bitbucket');
