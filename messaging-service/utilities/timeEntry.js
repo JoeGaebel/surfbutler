90fd0b16e7ecfbf1b710d9d3f15e30565e1486f1
Daniel Reissenberger <dreissenberger@atlassian.com>
2019-05-04 20:20:34 +1000

    Added possibility to get closest time entry for a custom time

--- messaging-service/utilities/timeEntry.js
+++ messaging-service/utilities/timeEntry.js
@@ -1,16 +1,20 @@
 const { getCurrentTimestamp } = require('./clock');
 
-exports.getClosest = (timeEntries) => {
-    const currentTimestamp = getCurrentTimestamp();
+exports.getClosest = (timeEntries, time) => {
     let closestElement = timeEntries[0];
     let smallestTimestampDelta = Infinity;
 
     timeEntries.forEach(element => {
-        if (currentTimestamp - element.timestamp < smallestTimestampDelta) {
+        if (time - element.timestamp < smallestTimestampDelta) {
             closestElement = element;
-            smallestTimestampDelta = Math.abs(currentTimestamp - element.timestamp);
+            smallestTimestampDelta = Math.abs(time - element.timestamp);
         }
     });
 
     return closestElement;
+};
+
+exports.getClosest = (timeEntries) => {
+    const currentTimestamp = getCurrentTimestamp();
+    return this.getClosest(timeEntries, currentTimestamp);
 };
