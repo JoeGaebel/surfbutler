6cb6b1a98816071dc586128ae3f1181fe8db123a
Daniel Reissenberger <dreissenberger@atlassian.com>
2019-05-18 19:12:17 +1000

    messageService publishes the message as a topic

--- messaging-service/messageService.js
+++ messaging-service/messageService.js
@@ -1,17 +1,13 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({ region: 'ap-southeast-2' });
-const phoneNumbers = ['+61439363614', '+61468921570'];
 
 exports.send = (message) => {
-    phoneNumbers.forEach((phoneNumber) => {
-        const params = { Message: message, PhoneNumber: phoneNumber };
-        const publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' })
-            .publish(params)
-            .promise();
+    const publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31', region: 'ap-southeast-2' })
+        .publish({ Message: message, TopicArn: 'arn:aws:sns:ap-southeast-2:077179938403:surf-messages' })
+        .promise();
 
-        publishTextPromise
-            .then(data => console.log('MessageID is ' + data.MessageId))
-            .catch(err => console.error(err, err.stack));
-    });
+    publishTextPromise
+        .then(data => console.log('MessageID is ' + data.MessageId))
+        .catch(err => console.error(err, err.stack));
 };
