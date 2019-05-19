df15050be301b88fc7158e051ec29f396d85041f
Daniel Reissenberger <dreissenberger@atlassian.com>
2019-05-19 10:32:39 +1000

    Added notifier

---
+++ marketing-resources/surf-butler-signup-notifier.js
@@ -0,0 +1,21 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'ap-southeast-2' });
+
+exports.handler = async (event, context) => {
+    for (const record of event.Records) {
+        if (record.eventName === 'INSERT') {
+            const message = "+" + record.dynamodb.NewImage.phoneNumber.S + " just signed up";
+            console.log(message);
+
+            const publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31', region: 'ap-southeast-2' })
+                .publish({ Message: message, TopicArn: 'arn:aws:sns:ap-southeast-2:077179938403:SurfButlerDevs' })
+                .promise();
+
+            publishTextPromise
+                .then(data => console.log('MessageID is ' + data.MessageId))
+                .catch(err => console.error(err, err.stack));
+        }
+    }
+    return `Successfully processed ${event.Records.length} records.`;
+};
