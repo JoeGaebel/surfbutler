c52dcd23834b9386217bc9677b76f997c774e9d1
Daniel Reissenberger <dreissenberger@atlassian.com>
2019-05-23 21:26:41 +1000

    DEBUG: Get segment ID

--- messaging-service/messageService.js
+++ messaging-service/messageService.js
@@ -6,5 +6,6 @@ AWS.config.update({ region: 'ap-southeast-2' });
 exports.send = ({ message, name }) => {
     const campaignSchema = createCampaignSchema(message, name);
     const pinpoint = new AWS.Pinpoint({ apiVersion: '2016-12-01' });
+    console.log(JSON.stringify(pinpoint.getSegments({ 'ApplicationId': 'efba3f1fc914421f88cb01c0efb16ffd' })));
     return pinpoint.createCampaign(campaignSchema, (err, data) => console.log(err, data)).promise();
 };
