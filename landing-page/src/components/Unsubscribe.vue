10ef0bf4a34e35faf9a2797cee36857d32ddc74d
Joe Gaebel <joe.gaebel@hotmail.com>
2019-06-01 18:42:26 +1000

    Add /unsubscribe

---
+++ landing-page/src/components/Unsubscribe.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="unsubscribe">
+        <iframe :src="iframeSrc">Loading...</iframe>
+    </div>
+</template>
+
+<script>
+const iframeSrc = 'https://docs.google.com/forms/d/e/'
+        + '1FAIpQLScWUvWgjx7arZVxnk01rThc1uwS6cWMgQZ5kfRiQHBly1MdFg/viewform?embedded=true';
+
+export default {
+    data: () => ({ iframeSrc }),
+};
+</script>
+
+<style scoped>
+    #unsubscribe {
+        margin-top: 2rem;
+        width: 500px;
+        height: 89vh;
+    }
+
+    iframe {
+        height: 100%;
+        width: 100%;
+    }
+
+    @media (max-width: 500px) {
+        #unsubscribe {
+            height: 101vh;
+            width: 100vw;
+            margin: 0.5rem 0;
+        }
+    }
+</style>
--- landing-page/src/main.js
+++ landing-page/src/main.js
@@ -1,18 +1,21 @@
+import '../node_modules/bulma/css/bulma.css';
+import 'vue-phone-number-input/dist/vue-phone-number-input.css';
+
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 
-import '../node_modules/bulma/css/bulma.css';
-import 'vue-phone-number-input/dist/vue-phone-number-input.css';
 import LandingPage from './components/LandingPage.vue';
 import Feedback from './components/Feedback.vue';
+import Unsubscribe from './components/Unsubscribe.vue';
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: LandingPage },
     { path: '/feedback', component: Feedback },
+    { path: '/unsubscribe', component: Unsubscribe },
+    { path: '*', component: LandingPage },
 ];
 
 const router = new VueRouter({ routes, mode: 'history' });
