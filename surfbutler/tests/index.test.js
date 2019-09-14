3b62be8398e4b1182c0f8e81414f56c2baf3db56
Joe Gaebel <joe.gaebel@hotmail.com>
2019-09-14 15:49:53 +1000

    Fix index.test so that it doesn't output console errors
     - Also add package-lock, as this is best practice to commit

--- surfbutler/tests/index.test.js
+++ surfbutler/tests/index.test.js
@@ -20,41 +20,51 @@ describe('handler', () => {
             'Bondi': 'bondi seg',
             'Tamarama': 'tam seg',
             'Bronte': 'bronte seg',
-            'Curl-Curl': 'curl curl seg'
+            'Curl-Curl': 'curl curl seg',
+            'Dee-Why': 'dee why seg',
+            'Freshwater': 'freshwater seg',
+            'Manly': 'manly seg'
         }));
 
         await handler();
 
-        expect(getSummary).toHaveBeenCalledWith('Bondi', '5842041f4e65fad6a7708bf8');
-        expect(getSummary).toHaveBeenCalledWith('Tamarama', '584204204e65fad6a77093eb');
-        expect(getSummary).toHaveBeenCalledWith('Bronte', '584204204e65fad6a77093ef');
+        expect(getSummary)
+            .toHaveBeenCalledWith('Bondi', '5842041f4e65fad6a7708bf8');
+        expect(getSummary)
+            .toHaveBeenCalledWith('Tamarama', '584204204e65fad6a77093eb');
+        expect(getSummary)
+            .toHaveBeenCalledWith('Bronte', '584204204e65fad6a77093ef');
 
-        expect(send).toHaveBeenCalledWith({
-            segment: 'bondi seg',
-            message: 'Bondi summary',
-            key: 'Bondi',
-            applicationId: 'efba3f1fc914421f88cb01c0efb16ffd'
-        });
+        expect(send)
+            .toHaveBeenCalledWith({
+                segment: 'bondi seg',
+                message: 'Bondi summary',
+                key: 'Bondi',
+                applicationId: 'efba3f1fc914421f88cb01c0efb16ffd'
+            });
 
-        expect(send).toHaveBeenCalledWith({
-            segment: 'tam seg',
-            message: 'Tamarama summary',
-            key: 'Tamarama',
-            applicationId: 'efba3f1fc914421f88cb01c0efb16ffd'
-        });
+        expect(send)
+            .toHaveBeenCalledWith({
+                segment: 'tam seg',
+                message: 'Tamarama summary',
+                key: 'Tamarama',
+                applicationId: 'efba3f1fc914421f88cb01c0efb16ffd'
+            });
 
-        expect(send).toHaveBeenCalledWith({
-            segment: 'bronte seg',
-            message: 'Bronte summary',
-            key: 'Bronte',
-            applicationId: 'efba3f1fc914421f88cb01c0efb16ffd'
-        });
+        expect(send)
+            .toHaveBeenCalledWith({
+                segment: 'bronte seg',
+                message: 'Bronte summary',
+                key: 'Bronte',
+                applicationId: 'efba3f1fc914421f88cb01c0efb16ffd'
+            });
 
-        expect(send).toHaveBeenCalledWith({
-            segment: 'curl curl seg',
-            message: 'Curl Curl summary',
-            key: 'Curl-Curl',
-            applicationId: 'efba3f1fc914421f88cb01c0efb16ffd'
-        });
+        expect(send)
+            .toHaveBeenCalledWith({
+                segment: 'curl curl seg',
+                message: 'Curl Curl summary',
+                key: 'Curl-Curl',
+                applicationId: 'efba3f1fc914421f88cb01c0efb16ffd'
+            });
     });
 });
